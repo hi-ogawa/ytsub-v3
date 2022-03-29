def7d129b64857db4ac0c8b01be82e5901d8ab3e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-03-29 13:14:41 +0900

    feat: add `/users/register` action (WIP)

--- app/routes/users/register.tsx
+++ app/routes/users/register.tsx
@@ -1,6 +1,71 @@
 import * as React from "react";
 import { Form, Link } from "@remix-run/react";
+import { ActionFunction } from "@remix-run/server-runtime";
+import { z } from "zod";
+import * as bcrypt from "bcryptjs";
 import { useIsFormValid } from "../../utils/hooks";
+import { fromRequestForm } from "../../utils/url-data";
+import { users } from "../../db/models";
+import { crypto } from "../../node.server";
+
+const USERNAME_MAX_LENGTH = 32;
+const PASSWORD_MAX_LENGTH = 128;
+const BCRYPT_ROUNDS = 10;
+
+const schema = z
+  .object({
+    username: z
+      .string()
+      .nonempty()
+      .max(USERNAME_MAX_LENGTH)
+      .regex(/^[a-zA-Z0-9_.-]+$/),
+    password: z.string().nonempty().max(PASSWORD_MAX_LENGTH),
+    passwordConfirmation: z.string().nonempty().max(PASSWORD_MAX_LENGTH),
+  })
+  .refine((obj) => obj.password === obj.passwordConfirmation);
+
+function prehash(password: string): string {
+  return crypto
+    .createHash("sha256")
+    .update(password, "utf8")
+    .digest()
+    .toString("base64");
+}
+
+async function toPasswordHash(password: string): Promise<string> {
+  return await bcrypt.hash(prehash(password), BCRYPT_ROUNDS);
+}
+
+// @ts-expect-error unused
+async function verifyPassword(
+  password: string,
+  passwordHash: string
+): Promise<boolean> {
+  return bcrypt.compare(prehash(password), passwordHash);
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const parsed = schema.safeParse(await fromRequestForm(request));
+  if (!parsed.success) {
+    return { message: "Invalid registration" };
+  }
+
+  // Verify uniqueness
+  const { data } = parsed;
+  if (await users().select().where("username", data.username).first()) {
+    return { message: `Username '${data.username}' is already taken` };
+  }
+
+  // Save
+  const passwordHash = await toPasswordHash(data.password);
+  const [userId] = await users().insert({
+    username: data.username,
+    passwordHash,
+  });
+
+  // TODO: login
+  return { userId };
+};
 
 export default function DefaultComponent() {
   const [isValid, formProps] = useIsFormValid();
@@ -17,6 +82,7 @@ export default function DefaultComponent() {
             name="username"
             className="input input-bordered"
             required
+            maxLength={USERNAME_MAX_LENGTH}
           />
         </div>
         <div className="form-control mb-6">
@@ -28,6 +94,19 @@ export default function DefaultComponent() {
             name="password"
             className="input input-bordered"
             required
+            maxLength={PASSWORD_MAX_LENGTH}
+          />
+        </div>
+        <div className="form-control mb-6">
+          <label className="label">
+            <span className="label-text">Password confirmation</span>
+          </label>
+          <input
+            type="password"
+            name="passwordConfirmation"
+            className="input input-bordered"
+            required
+            maxLength={PASSWORD_MAX_LENGTH}
           />
         </div>
         <div className="form-control">
