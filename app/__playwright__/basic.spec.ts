8c2195568d729efe7ace09c9a87a5f577cca1f42
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-03-31 13:11:33 +0900

    chore: cleanup comments

--- app/__playwright__/basic.spec.ts
+++ app/__playwright__/basic.spec.ts
@@ -23,7 +23,6 @@ test("setup => watch", async ({ page }) => {
   ).toHaveText("");
 
   // Select language options
-  // TODO: onChange is not triggered?
   await page
     .locator("data-test=setup-form >> select >> nth=0")
     .selectOption('{"id":".fr"}');
