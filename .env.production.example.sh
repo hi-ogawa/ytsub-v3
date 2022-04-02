3e7422fc1d8105fe251b15375e653cddc5fcd61c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-04-02 18:10:14 +0900

    chore: cleanup `.env.production.example.sh`

--- .env.production.example.sh
+++ .env.production.example.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# copy to e.g. https://app.netlify.com/sites/ytsub-v3-hiro18181/settings/deploys#environment
+
 # https://app.planetscale.com
 echo 'export APP_MYSQL_HOST=xxx'
 echo 'export APP_MYSQL_DATABASE=xxx'
@@ -8,4 +10,4 @@ echo 'export APP_MYSQL_PASSWORD=xxx'
 echo 'export APP_MYSQL_SSL=true'
 
 # openssl rand -hex 32
-echo 'export APP_SESSION_SECRET=d30eed9c045e6f54377860da2a8384341d3125623b3a5fa02c88d8bc3753ec17
+echo 'export APP_SESSION_SECRET=d30eed9c045e6f54377860da2a8384341d3125623b3a5fa02c88d8bc3753ec17'
