fde4dc40868083af3b47ced48ceb1e369d72b1a2
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-04-02 13:03:30 +0900

    refactor: move `app/env.ts`

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,5 +1,5 @@
 import { createCookieSessionStorage } from "@remix-run/server-runtime";
-import { env } from "../env";
+import { env } from "../misc/env";
 
 const { getSession, commitSession, destroySession } =
   createCookieSessionStorage({
