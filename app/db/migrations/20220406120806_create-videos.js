49e7b8a0a309123b3c27ec05831da9ba09531f70
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-04-06 21:14:13 +0900

    feat(migration): add "create-videos"

---
+++ app/db/migrations/20220406120806_create-videos.js
@@ -0,0 +1,32 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  await knex.raw(`
+    CREATE TABLE videos (
+      userId               BIGINT,
+      id                    BIGINT AUTO_INCREMENT,
+      videoId               VARCHAR(32) NOT NULL,
+      language1_id          VARCHAR(32) NOT NULL,
+      language1_translation VARCHAR(32) NOT NULL,
+      language2_id          VARCHAR(32) NOT NULL,
+      language2_translation VARCHAR(32) NOT NULL,
+      createdAt    DATETIME DEFAULT CURRENT_TIMESTAMP,
+      updatedAt    DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      PRIMARY KEY (id),
+      UNIQUE INDEX (videoId, language1_id, language1_translation, language2_id, language2_translation),
+      FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
+    );
+  `);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  await knex.raw(`
+    DROP TABLE videos;
+  `);
+};
