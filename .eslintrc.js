325280fd42879caf73a4d8b67b788c86c620bb31
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-04-06 21:22:40 +0900

    chore: cleanup eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,11 @@
 /** @type {import("@typescript-eslint/utils").TSESLint.Linter.Config} */
 module.exports = {
-  extends: ["prettier"],
   parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "tsconfig.json",
-    sourceType: "module",
-  },
-  plugins: ["eslint-plugin-import", "@typescript-eslint"],
+  plugins: ["@typescript-eslint", "eslint-plugin-import"],
+  extends: ["prettier"],
   rules: {
     "import/order": ["error"],
     "sort-imports": ["error", { ignoreDeclarationSort: true }],
   },
+  ignorePatterns: [".cache", "build", "node_modules"],
 };
