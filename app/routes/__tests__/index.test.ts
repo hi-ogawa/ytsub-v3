214998cfb02e69d2957ec4ef3626edc7727c5fbf
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-04-17 14:42:54 +0900

    test: update snapshot

--- app/routes/__tests__/index.test.ts
+++ app/routes/__tests__/index.test.ts
@@ -1,21 +1,21 @@
-import { describe, expect, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
+import { tables } from "../../db/models";
 import { assert } from "../../misc/assert";
-import { loader } from "../bookmarks/index";
-import { testLoader, useUser } from "./helper";
+import { loader } from "../index";
+import { testLoader } from "./helper";
 
 // TODO: use data
-describe("bookmarks/index.loader", () => {
-  const { signin } = useUser({
-    seed: __filename,
+describe("index.loader", () => {
+  beforeAll(async () => {
+    await tables.videos().delete();
   });
 
   it("basic", async () => {
-    const res = await testLoader(loader, { transform: signin });
+    const res = await testLoader(loader);
     assert(res instanceof Response);
     expect(await res.json()).toMatchInlineSnapshot(`
       {
         "json": {
-          "captionEntries": [],
           "pagination": {
             "data": [],
             "page": 1,
@@ -23,7 +23,6 @@ describe("bookmarks/index.loader", () => {
             "total": 0,
             "totalPage": 0,
           },
-          "videos": [],
         },
       }
     `);
