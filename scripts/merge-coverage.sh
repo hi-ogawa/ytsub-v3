b3fe7ae398f4d462e2d4dc11f366f139a696bf60
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-04-30 16:59:35 +0900

    fix(test): use inline sourcemap for server build (#73)
    * fix(test): use inline sourcemap for server build
    
    * chore: add "scripts/merge-coverage.sh"
    
    * chore(ci): remove tmp json before publishing coverage

---
+++ scripts/merge-coverage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eux -o pipefail
+
+rm -rf coverage/all
+mkdir -p coverage/all/tmp
+
+find coverage -type f \
+  -path '*/unit/tmp/*.json' -o \
+  -path '*/e2e-server/tmp/*.json' -o \
+  -path '*/e2e-server/tmp/*.json' \
+  -exec cp -t coverage/all/tmp {} +
+
+shopt -u nullglob
+for test_type in unit e2e-server e2e-client; do
+  for filename in coverage/"$test_type"/tmp/*.json; do
+    cp "$filename" "coverage/all/tmp/$test_type--$(basename "$filename")"
+  done
+done
+
+npx c8 report -o coverage/all -r text -r html --all --src app --exclude build --exclude-after-remap --temp-directory coverage/all/tmp
