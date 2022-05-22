a38a1c15fe454a53d3e24a4406d09594e4eb72bd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-05-22 19:45:46 +0900

    fix: fix scripts/vercel.sh

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -25,5 +25,6 @@ npx esbuild build/remix/production/server/index.js --outfile=build/remix/product
   --bundle --platform=node \
   --external:mysql
 
-# setup vercel-cli-deploy-able files
+# setup vercel-cli deployable directory
+mkdir -p .vercel
 cp -rf vercel.json .vercel build/remix/production/public build/remix/production/vercel
