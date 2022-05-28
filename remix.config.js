0d49692066083bd733a79caf97fe82d349d6ffcf
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-05-28 12:37:07 +0900

    chore: remove netlify (#144)
    * chore: cleanup .env.example
    
    * chore: fix main database
    
    * chore: cleanup "APP_DEFINE_STAGING"
    
    * chore: remove netlify
    
    * fix: fix scripts/vercel.sh

--- remix.config.js
+++ remix.config.js
@@ -4,10 +4,6 @@ const env = process.env.NODE_ENV ?? "development";
 module.exports = {
   serverBuildPath: `build/remix/${env}/server/index.js`,
   assetsBuildDirectory: `build/remix/${env}/public/build`, // @remix-run/serve is patched to serve this directory
-  server: process.env.BUILD_VERCEL
-    ? "./app/misc/vercel.ts"
-    : process.env.BUILD_NETLIFY
-    ? "./app/misc/netlify.ts"
-    : undefined,
+  server: process.env.BUILD_VERCEL ? "./app/misc/vercel.ts" : undefined,
   ignoredRouteFiles: ["**/__tests__/**/*"],
 };
