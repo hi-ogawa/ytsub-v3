2b5931639e5c97f1d9dc6cc7ec4d89df70275084
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-07-09 17:38:55 +0900

    perf: add index `practiceActions.createdAt` (#163)
    * perf: create index practiceActions.createdAt
    
    * chore: update schema
    
    * chore: update snapshot

---
+++ app/db/migrations/20220709082116_create-index-practiceActions-createdAt.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  await knex.raw(`
+    ALTER TABLE practiceActions ADD KEY practiceActions_createdAt_key (createdAt)
+  `);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  await knex.raw(`
+    ALTER TABLE practiceActions DROP KEY practiceActions_createdAt_key
+  `);
+};
