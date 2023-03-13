57383c2e2c106f31eecc59ec29f1b33841f028d6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-03-13 13:48:50 +0900

    chore: add `bar-of-progress` (#171)

--- app/components/top-progress-bar.tsx
+++ app/components/top-progress-bar.tsx
@@ -1,68 +1,23 @@
-import { Transition } from "@headlessui/react";
+import BarOfProgress from "@badrap/bar-of-progress";
 import React from "react";
-import { usePrevious } from "react-use";
-import { useRafTime } from "../utils/hooks";
-
-const FINISH_DURATION = 500;
 
 export function TopProgressBar({ loading }: { loading: boolean }) {
-  const prev = usePrevious(loading);
-  const [showing, setShowing] = React.useState(false);
-  const [finishing, setFinishing] = React.useState(false);
-  const [time, startTime, endTime] = useRafTime();
+  const [barOfProgress] = React.useState(
+    () =>
+      new BarOfProgress({
+        size: 3,
+        // daisy-ui secondary
+        color: "hsl(314 100% 47%)",
+      })
+  );
 
   React.useEffect(() => {
-    let clear: any;
-    if (!loading) {
-      if (showing) {
-        setFinishing(true);
-        const timer = setTimeout(() => setShowing(false), FINISH_DURATION);
-        clear = () => clearTimeout(timer);
-      }
-      endTime();
+    if (loading) {
+      barOfProgress.start();
+    } else {
+      barOfProgress.finish();
     }
-    if (loading && !prev) {
-      setFinishing(false);
-      setShowing(true);
-      startTime();
-    }
-    return clear;
   }, [loading]);
 
-  return (
-    <Transition
-      as={React.Fragment}
-      show={showing}
-      leave="transition-opacity duration-250"
-      leaveFrom="opacity-100"
-      leaveTo="opacity-0"
-    >
-      <div className={`absolute top-0 z-50 w-full`}>
-        <div className="absolute border-b-[3px] w-full border-secondary/[.3]" />
-        <div
-          className="absolute border-b-[3px] w-full shadow-sm border-secondary"
-          style={{
-            transformOrigin: "0 0",
-            transform: finishing
-              ? "scaleX(1)"
-              : `scaleX(${computeProgress(time / 1000)})`,
-            transitionProperty: finishing ? "transform" : "none",
-            transitionDuration: finishing ? String(FINISH_DURATION) : "0",
-          }}
-        />
-      </div>
-    </Transition>
-  );
-}
-
-function computeProgress(s: number): number {
-  // C1 function [0, oo) -> [0, 1)
-
-  // linear: [0, 1] -> [0, 0.5]
-  if (s < 1) {
-    return 0.5 * s;
-  }
-
-  // tanh: [1, oo) -> [0.5, 1)
-  return 0.5 + 0.5 * Math.tanh(s - 1);
+  return null;
 }
