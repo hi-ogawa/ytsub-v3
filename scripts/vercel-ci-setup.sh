4669a93b6a663d4164906e92afe2edfa28d44227
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-03-23 12:44:06 +0900

    ci: add release.yml

---
+++ scripts/vercel-ci-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu -o pipefail
+
+# copied from https://github.com/hi-ogawa/unocss-preset-antd/blob/1e096a1e2b8bbf6193bb6f0fb33e225ca355fb6e/packages/app/misc/vercel/ci-setup.sh#L6-L15
+
+config_dir="$HOME/.local/share/com.vercel.cli"
+mkdir -p "$config_dir"
+
+cat > "$config_dir/auth.json" <<EOF
+{
+  "token": "$VERCEL_TOKEN"
+}
+EOF
+
+cat > "$config_dir/config.json" <<EOF
+{}
+EOF
