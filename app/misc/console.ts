b466b1382440e2ea06a35b6a979e12ac2001aa7b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-02 00:39:52 +0900

    refactor: update console.ts

--- app/misc/console.ts
+++ app/misc/console.ts
@@ -1,7 +1,22 @@
+import repl from "node:repl";
 import { client } from "../db/client.server";
+import { E, T, db } from "../db/drizzle-client.server";
 import { Q } from "../db/models";
+import { initializeServer } from "./initialize-server";
 
-Object.assign(global, {
-  client,
-  Q,
-});
+async function main() {
+  await initializeServer();
+  const replServer = repl.start();
+  Object.assign(replServer.context, {
+    client,
+    Q,
+    T,
+    E,
+    db,
+  });
+  replServer.on("exit", () => {
+    process.exit(1);
+  });
+}
+
+main();
