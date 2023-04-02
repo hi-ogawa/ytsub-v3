37a19438db6fda48a5677b8eefc5b50b99208000
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-02 09:11:31 +0900

    chore: define drizzle schema

--- app/db/drizzle-client.server.ts
+++ app/db/drizzle-client.server.ts
@@ -2,8 +2,11 @@ import { once } from "@hiogawa/utils";
 import * as E from "drizzle-orm/expressions";
 import {
   InferModel,
+  boolean,
   customType,
+  float,
   int,
+  json,
   mysqlTable,
   serial,
   text,
@@ -12,6 +15,7 @@ import { MySql2Database, drizzle } from "drizzle-orm/mysql2";
 import { createConnection } from "mysql2/promise";
 import { throwGetterProxy } from "../utils/misc";
 import knexfile from "./knexfile.server";
+import type { PracticeActionType, PracticeQueueType } from "./models";
 
 //
 // schema utils
@@ -33,33 +37,119 @@ const datetimeUtc = customType<{ data: Date; driverData: string }>({
   },
 });
 
+const timestampColumns = {
+  createdAt: datetimeUtc("createdAt").notNull().default(DUMMY_DEFAULT),
+  updatedAt: datetimeUtc("updatedAt").notNull().default(DUMMY_DEFAULT),
+};
+
 //
 // schema
 //
 
 const users = mysqlTable("users", {
   id: serial("id").primaryKey(),
+  ...timestampColumns,
+  //
   username: text("username").notNull(), // TODO: case insensitive
   passwordHash: text("passwordHash").notNull(), // TODO: reduct when responding to client
-  createdAt: datetimeUtc("createdAt").notNull().default(DUMMY_DEFAULT),
-  updatedAt: datetimeUtc("updatedAt").notNull().default(DUMMY_DEFAULT),
   language1: text("language1"),
   language2: text("language2"),
   timezone: text("timezone").notNull().default(DUMMY_DEFAULT),
 });
 
-export type User = InferModel<typeof users>;
+const videos = mysqlTable("videos", {
+  id: serial("id").primaryKey(),
+  userId: int("userId"), // `null` indicates the video is created by/for "anonymous" users
+  ...timestampColumns,
+  //
+  videoId: text("videoId").notNull(),
+  language1_id: text("language1_id").notNull(),
+  language2_id: text("language2_id").notNull(),
+  language1_translation: text("language1_translation"),
+  language2_translation: text("language2_translation"),
+  title: text("title").notNull(),
+  author: text("author").notNull(),
+  channelId: text("channelId").notNull(),
+  bookmarkEntriesCount: int("bookmarkEntriesCount").notNull(),
+});
 
+const captionEntries = mysqlTable("captionEntries", {
+  id: serial("id").primaryKey(),
+  videoId: int("videoId").notNull(), // not `videos.videoId` but `videos.id`
+  ...timestampColumns,
+  //
+  index: int("index").notNull(), // zero-based index within video's caption entries
+  begin: int("begin"),
+  end: int("end"),
+  text1: text("text1"),
+  text2: text("text2"),
+});
+
+const bookmarkEntries = mysqlTable("bookmarkEntries", {
+  id: serial("id").primaryKey(),
+  userId: int("userId"), // `null` indicates the video is created by/for "anonymous" users
+  videoId: text("videoId").notNull(),
+  ...timestampColumns,
+  //
+  language1_id: text("language1_id").notNull(),
+  language2_id: text("language2_id").notNull(),
+  language1_translation: text("language1_translation"),
+  language2_translation: text("language2_translation"),
+  title: text("title").notNull(),
+  author: text("author").notNull(),
+  channelId: text("channelId").notNull(),
+  bookmarkEntriesCount: int("bookmarkEntriesCount").notNull(),
+});
+
+// prettier-ignore
 const decks = mysqlTable("decks", {
   id: serial("id").primaryKey(),
   userId: int("userId").notNull(),
-  // TODO
+  ...timestampColumns,
+  //
+  newEntriesPerDay: int("newEntriesPerDay").notNull(),
+  reviewsPerDay: int("reviewsPerDay").notNull(),
+  easeMultiplier: float("easeMultiplier").notNull(),
+  easeBonus: float("easeBonus").notNull(),
+  randomMode: boolean("randomMode").notNull(),
+  practiceEntriesCountByQueueType: json<Record<PracticeQueueType, number>>("practiceEntriesCountByQueueType").notNull(),
 });
 
-export type Deck = InferModel<typeof decks>;
+const practiceEntries = mysqlTable("practiceEntries", {
+  id: serial("id").primaryKey(),
+  deckId: int("deckId").notNull(),
+  bookmarkEntryId: int("bookmarkEntryId").notNull(),
+  ...timestampColumns,
+  //
+  queueType: text<PracticeQueueType>("queueType").notNull(),
+  easeFactor: float("easeFactor").notNull(),
+  scheduledAt: datetimeUtc("scheduledAt").notNull(),
+  practiceActionsCount: int("practiceActionsCount").notNull(),
+});
+
+const practiceActions = mysqlTable("practiceActions", {
+  id: serial("id").primaryKey(),
+  deckId: int("deckId").notNull(),
+  practiceEntryId: int("practiceEntryId").notNull(),
+  userId: int("userId").notNull(),
+  ...timestampColumns,
+  //
+  queueType: text<PracticeQueueType>("queueType").notNull(),
+  actionType: text<PracticeActionType>("actionType").notNull(),
+});
 
 // short accessor for tables
-export const T = { users, decks };
+export const T = {
+  users,
+  videos,
+  captionEntries,
+  bookmarkEntries,
+  decks,
+  practiceEntries,
+  practiceActions,
+};
+
+export type TT = { [K in keyof typeof T]: InferModel<(typeof T)[K]> };
 
 //
 // utils
