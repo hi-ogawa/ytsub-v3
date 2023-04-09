b31c65d072c1ff91e83bfac3de493d520398c754
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-09 18:49:39 +0900

    test: fix testTrpcClient

--- app/trpc/test-helper.ts
+++ app/trpc/test-helper.ts
@@ -4,7 +4,7 @@ import { createTrpcAppContext } from "./context";
 import { trpcApp } from "./server";
 
 export async function testTrpcClient(options?: { user?: TT["users"] }) {
-  const req = new Request("/dummy");
+  const req = new Request("http://example.com"); // dummy
   if (options?.user) {
     const cookie = await createUserCookie(options.user);
     req.headers.set("cookie", cookie);
