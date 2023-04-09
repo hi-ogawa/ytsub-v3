87f85b7363e7603048b4597a525a59b3b1e84a88
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-09 18:24:40 +0900

    fix: fix publicPath

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,14 @@
 patch();
 
 const env = process.env.NODE_ENV ?? "development";
-const buildDir = `build/remix/${env}/`;
 
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildPath: buildDir + `server/index.js`,
-  assetsBuildDirectory: buildDir + `public/build`,
-  publicPath: buildDir + `public/build`,
+  serverBuildPath: `build/remix/${env}/server/index.js`,
+  assetsBuildDirectory: `build/remix/${env}/public/build`,
+  // TOOD: only for dev
+  // TODO: fix "/_copy" assets
+  publicPath: `/build/remix/${env}/public/build`,
   server: process.env.BUILD_VERCEL ? "./app/misc/vercel.ts" : undefined,
   ignoredRouteFiles: ["**/*.test.ts"],
   future: {
