99fc4c66a6c53f765456036d99153763c986e657
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-09 17:16:07 +0900

    chore: fix ignoredRouteFiles (wip)

--- remix.config.js
+++ remix.config.js
@@ -7,12 +7,13 @@ module.exports = {
   assetsBuildDirectory: buildDir + `public/build`,
   publicPath: buildDir + `public/build`,
   server: process.env.BUILD_VERCEL ? "./app/misc/vercel.ts" : undefined,
-  ignoredRouteFiles: ["**/*.test.*"],
-  routes: require("@remix-run/v1-route-convention").createRoutesFromFolders,
+  ignoredRouteFiles: ["**/*.test.ts"],
   future: {
     v2_meta: true,
     v2_errorBoundary: true,
     v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
+    // TODO: doesn't work with `ignoredRouteFiles` options?
+    // v2_routeConvention: true,
   },
+  // routes: require("@remix-run/v1-route-convention").createRoutesFromFolders,
 };
