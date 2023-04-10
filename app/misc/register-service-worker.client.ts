decf172a90348ec988cdee5235f9c3466f7f5f59
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-10 12:36:40 +0900

    chore(deps): upgrade remix (#242)

--- app/misc/register-service-worker.client.ts
+++ app/misc/register-service-worker.client.ts
@@ -1,12 +1,10 @@
-// see scripts/copy-assets.sh
 const SERVICE_WORKER_PATH = "/service-worker.js";
 
 export function registerServiceWorker() {
   const { serviceWorker } = window.navigator;
   if (serviceWorker) {
     window.addEventListener("load", async () => {
-      const registration = await serviceWorker.register(SERVICE_WORKER_PATH);
-      console.debug("service worker registration success", registration);
+      await serviceWorker.register(SERVICE_WORKER_PATH);
     });
   }
 }
