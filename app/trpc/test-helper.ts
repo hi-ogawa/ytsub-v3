e3b529565fc9a1e0c19ac239c3e5b506d862c21e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-15 21:12:18 +0900

    refactor: minor

--- app/trpc/test-helper.ts
+++ app/trpc/test-helper.ts
@@ -3,17 +3,11 @@ import { createUserCookie } from "../utils/auth";
 import { createTrpcAppContext } from "./context";
 import { trpcApp } from "./server";
 
-export async function testTrpcClient(options?: { user?: TT["users"] }) {
-  const req = new Request("http://example.com"); // dummy
-  if (options?.user) {
-    const cookie = await createUserCookie(options.user);
-    req.headers.set("cookie", cookie);
-  }
-  const ctx = await createTrpcAppContext({
-    req,
-    resHeaders: new Response().headers,
-  });
-  return trpcApp.createCaller(ctx);
+export async function testTrpcClient(
+  ...args: Parameters<typeof testTrpcClientWithContext>
+) {
+  const { caller } = await testTrpcClientWithContext(...args);
+  return caller;
 }
 
 export async function testTrpcClientWithContext(options?: {
