2ef127464ff8587095f23259b2e9c6a764a13951
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-15 14:38:14 +0900

    chore: setup source maps (#263)

--- app/misc/vercel.ts
+++ app/misc/vercel.ts
@@ -1,4 +1,6 @@
 import * as build from "@remix-run/dev/server-build";
 import { createRequestHandler } from "@remix-run/vercel";
 
+// TODO: @remix-run/vercel is deprecated https://github.com/remix-run/remix/pull/5964
+
 export default createRequestHandler({ build });
---
+++ misc/build/bundle-vercel.ts
@@ -0,0 +1,47 @@
+import fs from "node:fs";
+import esbuild from "esbuild";
+
+// used by scripts/vercel.sh to
+// - bundle server app for simpler vercel deployment
+// - exclude node_modules from generating source map to reduce deployment size
+
+// references
+// - https://esbuild.github.io/plugins/
+// - https://github.com/evanw/esbuild/issues/1685#issuecomment-944916409
+
+esbuild.build({
+  logLevel: "info",
+  entryPoints: ["./build/remix/production/server/index.js"],
+  outfile: "./build/remix/production/server/index-bundled.js",
+  bundle: true,
+  sourcemap: "inline",
+  platform: "node",
+  external: [
+    // exclude knex drivers except "mysql2"
+    "mysql",
+    "sqlite3",
+    "better-sqlite3",
+    "tedious",
+    "pg",
+    "oracledb",
+    "pg-query-stream",
+  ],
+  plugins: [
+    // https://github.com/evanw/esbuild/issues/1685#issuecomment-944916409
+    {
+      name: "no-source-map-node-modules",
+      setup(build) {
+        build.onLoad({ filter: /node_modules/ }, (args) => {
+          if (args.path.endsWith("js")) {
+            return {
+              contents:
+                fs.readFileSync(args.path, "utf8") +
+                "\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIiJdLCJtYXBwaW5ncyI6IkEifQ==",
+              loader: "default",
+            };
+          }
+        });
+      },
+    },
+  ],
+});
