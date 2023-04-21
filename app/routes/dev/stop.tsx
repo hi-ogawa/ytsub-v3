1168797ab7396112abbeb8fea2ba0b5b7b9fb39b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-21 15:32:35 +0900

    chore: comment

--- app/routes/dev/stop.tsx
+++ app/routes/dev/stop.tsx
@@ -1,11 +1,13 @@
 import process from "process";
 import type { LoaderFunction } from "@remix-run/server-runtime";
 
+// gracefully shutdown itself for scripts/test-e2e-coverage.sh
+
 export const loader: LoaderFunction = async () => {
   if (process.env.NODE_ENV === "production") {
     return { success: false };
   }
-  console.log("🔥 process.kill(process.pid) 🔥");
+  console.log("🔥 process.kill 🔥");
   process.kill(process.pid, "SIGINT");
   return { success: true };
 };
