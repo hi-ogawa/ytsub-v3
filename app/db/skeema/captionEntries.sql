9bec94460e4be247e608af14e7aaf2bbcb0ef237
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-22 14:09:21 +0900

    chore: pull

--- app/db/skeema/captionEntries.sql
+++ app/db/skeema/captionEntries.sql
@@ -9,5 +9,6 @@ CREATE TABLE `captionEntries` (
   `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `videoId` bigint NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `captionEntries_videoId_index_key` (`videoId`, `index`)
+  KEY `captionEntries_videoId_key` (`videoId`),
+  KEY `captionEntries_videoId_index_key` (`videoId`,`index`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
