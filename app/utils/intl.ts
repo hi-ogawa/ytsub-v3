63fd46ffb5cc4b1bab2ce05da052a55d07aa740c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-28 17:39:22 +0900

    feat: add formatRelativeDate

--- app/utils/intl.ts
+++ app/utils/intl.ts
@@ -1,3 +1,7 @@
+import { toInstant, toZdt } from "./temporal-utils";
+
+// TODO(refactor): should do something with temporal-utils (e.g. dealing with default/system timezone)
+
 export const dtf = new Intl.DateTimeFormat("en-US", {
   dateStyle: "long",
   timeStyle: "long",
@@ -9,3 +13,27 @@ export const dtfDateOnly = new Intl.DateTimeFormat("en-US", {
 });
 
 export const rtf = new Intl.RelativeTimeFormat("en-US");
+
+// DateTimeFormat with a special formatting for recent date e.g. "today", "yesterday", ...
+export function formatRelativeDate(
+  date: Date,
+  now: Date,
+  timeZone?: string // for testing
+): string {
+  const dtf = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "long",
+    timeStyle: "medium",
+    hour12: false,
+    timeZone,
+  });
+  const rtf = new Intl.RelativeTimeFormat("en-US", { numeric: "auto" });
+
+  let result = dtf.format(date);
+  const startOfToday = toZdt(now, dtf.resolvedOptions().timeZone).startOfDay();
+  const days = toInstant(date).since(startOfToday.toInstant()).total("days");
+  const daysIntegral = Math.floor(days);
+  if (Math.abs(daysIntegral) < 2) {
+    result = result.replace(/^.*at/, rtf.format(daysIntegral, "days") + " at");
+  }
+  return result;
+}
