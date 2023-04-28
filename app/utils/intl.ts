739f6b4b1804efdb5fa6758db6f165068b4f722f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-28 22:41:56 +0900

    chore: comment

--- app/utils/intl.ts
+++ app/utils/intl.ts
@@ -1,7 +1,9 @@
 import { capitalize } from "lodash";
 import { toInstant, toZdt } from "./temporal-utils";
 
-// TODO(refactor): should do something with temporal-utils (e.g. dealing with default/system timezone)
+// TODO(refactor)
+// should do something with temporal-utils (e.g. dealing with default/system timezone)
+// or probably just use format-js
 
 export const dtf = new Intl.DateTimeFormat("en-US", {
   dateStyle: "long",
@@ -15,7 +17,7 @@ export const dtfDateOnly = new Intl.DateTimeFormat("en-US", {
 
 export const rtf = new Intl.RelativeTimeFormat("en-US");
 
-// DateTimeFormat with a special formatting for recent date e.g. "today", "yesterday", ...
+// DateTimeFormat with a special formatting for certain dates e.g. "today", "yesterday", ...
 export function formatRelativeDate(
   date: Date,
   // for testing
@@ -30,10 +32,11 @@ export function formatRelativeDate(
   });
   const rtf = new Intl.RelativeTimeFormat("en-US", { numeric: "auto" });
 
-  let result = dtf.format(date);
   const startOfToday = toZdt(now, dtf.resolvedOptions().timeZone).startOfDay();
   const days = toInstant(date).since(startOfToday.toInstant()).total("days");
   const daysIntegral = Math.floor(days);
+
+  let result = dtf.format(date);
   if (Math.abs(daysIntegral) < 2) {
     result = result.replace(
       /^.*at/,
