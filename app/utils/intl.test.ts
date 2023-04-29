0c94c4c995147a271aa6fef4b5df6691f8ebd3ca
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-04-29 14:01:56 +0900

    refactor: use format-js for formatting (#325)

--- app/utils/intl.test.ts
+++ app/utils/intl.test.ts
@@ -1,18 +1,21 @@
-import { describe, expect, it } from "vitest";
-import { formatRelativeDate } from "./intl";
+import { beforeAll, describe, expect, it } from "vitest";
+import { formatRelativeDate, mockTimezone } from "./intl";
 import { fromTemporal, toInstant } from "./temporal-utils";
+
+beforeAll(() => {
+  mockTimezone("Asia/Tokyo");
+});
 
 describe("formatRelativeDate", () => {
   // prettier-ignore
   it("basic", () => {
     const now = new Date("2023-04-28T08:35:36.003Z");
-    const timeZone = "Asia/Tokyo"
-    expect(formatRelativeDate(now, now, timeZone)).toMatchInlineSnapshot('"Today at 17:35:36"');
-    expect(formatRelativeDate(fromTemporal(toInstant(now).add({ hours: +40 })), now, timeZone)).toMatchInlineSnapshot('"April 30, 2023 at 09:35:36"');
-    expect(formatRelativeDate(fromTemporal(toInstant(now).add({ hours: +30 })), now, timeZone)).toMatchInlineSnapshot('"Tomorrow at 23:35:36"');
-    expect(formatRelativeDate(fromTemporal(toInstant(now).add({ hours:  +6 })), now, timeZone)).toMatchInlineSnapshot('"Today at 23:35:36"');
-    expect(formatRelativeDate(fromTemporal(toInstant(now).add({ hours: -16 })), now, timeZone)).toMatchInlineSnapshot('"Today at 01:35:36"');
-    expect(formatRelativeDate(fromTemporal(toInstant(now).add({ hours: -30 })), now, timeZone)).toMatchInlineSnapshot('"Yesterday at 11:35:36"');
-    expect(formatRelativeDate(fromTemporal(toInstant(now).add({ hours: -50 })), now, timeZone)).toMatchInlineSnapshot('"April 26, 2023 at 15:35:36"');
+    expect(formatRelativeDate(now, now)).toMatchInlineSnapshot('"today, 17:35:36"');
+    expect(formatRelativeDate(fromTemporal(toInstant(now).add({ hours: +40 })), now)).toMatchInlineSnapshot('"Apr 30, 2023, 09:35:36"');
+    expect(formatRelativeDate(fromTemporal(toInstant(now).add({ hours: +30 })), now)).toMatchInlineSnapshot('"tomorrow, 23:35:36"');
+    expect(formatRelativeDate(fromTemporal(toInstant(now).add({ hours:  +6 })), now)).toMatchInlineSnapshot('"today, 23:35:36"');
+    expect(formatRelativeDate(fromTemporal(toInstant(now).add({ hours: -16 })), now)).toMatchInlineSnapshot('"today, 01:35:36"');
+    expect(formatRelativeDate(fromTemporal(toInstant(now).add({ hours: -30 })), now)).toMatchInlineSnapshot('"yesterday, 11:35:36"');
+    expect(formatRelativeDate(fromTemporal(toInstant(now).add({ hours: -50 })), now)).toMatchInlineSnapshot('"Apr 26, 2023, 15:35:36"');
   });
 });
