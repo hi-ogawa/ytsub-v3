3e33fd7135f958603bc309bb7ca83900ec313a0b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-04 08:43:33 +0900

    chore(deps): update utils (#354)

--- app/utils/misc.ts
+++ app/utils/misc.ts
@@ -1,4 +1,4 @@
-import { groupBy, mapValues, range, tinyassert } from "@hiogawa/utils";
+import { tinyassert, zip } from "@hiogawa/utils";
 
 export function createGetProxy(
   propHandler: (prop: string | symbol) => unknown
@@ -26,19 +26,6 @@ export function usePromiseQueryOpitons<T>(queryFn: () => Promise<T>) {
     queryKey: ["usePromise", String(queryFn)],
     queryFn,
   };
-}
-
-// TODO: to utils
-export function assertUnreachable(_value: never): never {
-  tinyassert(false, "unreachable");
-}
-
-export function mapGroupBy<T, K, V>(
-  ls: T[],
-  keyFn: (v: T) => K,
-  valueFn: (vs: T[]) => V
-) {
-  return mapValues(groupBy(ls, keyFn), valueFn);
 }
 
 export function objectFromMap<K extends keyof any, V>(
@@ -74,25 +61,9 @@ export function defaultObject<K extends string, Keys extends [K, ...K[]], V>(
   return Object.fromEntries(keys.map((t) => [t, defaultValue])) as any;
 }
 
-export function capitalize(s: string): string {
-  return s.slice(0, 1).toUpperCase() + s.slice(1);
-}
-
 // shortcut for `undefined as T | undefined`
 export function none<T>(): T | undefined {
   return undefined;
-}
-
-export function zip<T1, T2>(
-  ls1: readonly T1[],
-  ls2: readonly T2[]
-): [T1, T2][] {
-  return range(Math.min(ls1.length, ls2.length)).map((i) => [ls1[i], ls2[i]]);
-}
-
-export function difference<T>(ls1: T[], ls2: T[]): T[] {
-  const exclude = new Set(ls2);
-  return ls1.filter((e) => !exclude.has(e));
 }
 
 // new RegExp(String.raw`...`) with only inner strings are escaped
