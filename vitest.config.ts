c2af4a5b9290ee0225f7d585310e309344857c7d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-04 09:46:50 +0900

    test: run unit test in parallel

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,8 +8,6 @@ export default defineConfig({
     environment: "happy-dom",
     globalSetup: ["./app/misc/test-setup-global.ts"],
     setupFiles: ["./app/misc/test-setup.ts"],
-    // disable parallel execution since it's not trivial to deal with stateful modules and database uniqueness
-    threads: false,
     coverage: {
       reporter: ["text", "html"],
       reportsDirectory: "coverage/unit",
