2dd9e0235bd3acb9ac1b84b0acbbb5317be31c8f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-06 15:31:27 +0900

    wip

---
+++ app/misc/entry-express.ts
@@ -0,0 +1,11 @@
+// import type { HattipHandler } from "@hattip/core";
+// import * as build from "@remix-run/dev/server-build";
+// import { createRequestHandler } from "@remix-run/express"
+
+// const remixHandler = createRequestHandler({ build });
+
+// const hattipHandler: HattipHandler = (context) => {
+//   return remixHandler(context.request);
+// };
+
+// export default hattipHandler;
