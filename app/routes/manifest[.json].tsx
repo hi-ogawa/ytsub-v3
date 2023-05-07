b0ce17cf48ffd011d8e4ba8e48db296e93656dfa
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-07 11:05:27 +0900

    fix: fix superjson

--- app/routes/manifest[.json].tsx
+++ app/routes/manifest[.json].tsx
@@ -1,10 +1,11 @@
+import { prettierJson } from "../utils/loader-utils";
 import { makeLoader } from "../utils/loader-utils.server";
 
 // it could be moved to `/public` but the loader conveniently works for now.
 
 export const loader = makeLoader(({ ctx }) => {
   ctx.cacheResponse();
-  return MANIFEST_JSON;
+  return prettierJson(MANIFEST_JSON);
 });
 
 const MANIFEST_JSON = {
