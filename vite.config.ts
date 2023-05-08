f86a08c0ce98984bc2d4f0a5462eb1e7fe740ab3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-08 20:49:56 +0900

    refactor: simplify theme script (#377)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,31 @@
+import fs from "node:fs";
 import react from "@vitejs/plugin-react";
 import unocss from "unocss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [unocss(), react()],
+  plugins: [unocss(), react(), injectHtmlPlugin()],
   clearScreen: false,
   publicDir: false,
 });
+
+// inject theme initialization script
+function injectHtmlPlugin() {
+  const script = fs.readFileSync(
+    require.resolve("@hiogawa/utils-experimental/dist/theme-script.global.js"),
+    "utf-8"
+  );
+  return {
+    name: "local:" + injectHtmlPlugin.name,
+    transformIndexHtml(html: string) {
+      return html.replace(
+        /<!--@@INJECT_THEME_SCRIPT@@-->/,
+        `\
+<script>
+  globalThis.__themeStorageKey = "ytsub:theme";
+  ${script}
+</script>`
+      );
+    },
+  };
+}
