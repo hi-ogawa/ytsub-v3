c5b8550fd90c3a6695b7ebedf9189de264e6e838
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-08 12:47:03 +0900

    refactor: add loaderOverridePlugin

--- remix.config.js
+++ remix.config.js
@@ -19,3 +19,63 @@ module.exports = {
       { ignoredFilePatterns: ["**/*.test.*"] }
     ),
 };
+
+// see patches/@remix-run__dev
+globalThis.__esbuildPluginsCommon = [loaderOverridePlugin()];
+globalThis.__esbuildPluginsBrowser = [pureCommentPlugin()];
+globalThis.__esbuildPluginsServer = [];
+
+function pureCommentPlugin() {
+  return {
+    name: pureCommentPlugin.name,
+    setup(build) {
+      // TODO
+      // build.onLoad({
+      //   // match only route file import
+      //   namespace: "file",
+      //   filter: new RegExp(Object.values(config.routes).map(({ file }) => `${config.appDirectory}/${file}`.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join("|"))
+      // }, async args => {
+      //   var fs = require('node:fs');
+      //   var path = require('node:path');
+      //   var loaders = require('../loaders.js');
+      //   let contents = await fs.promises.readFile(args.path, "utf8");
+      //   contents = contents.replace("= makeLoader", "= /* @__PURE__ */ makeLoader");
+      //   return {
+      //     contents,
+      //     loader: loaders.getLoaderForFile(args.path),
+      //     resolveDir: path.dirname(args.path)
+      //   };
+      // })
+    },
+  };
+}
+
+function loaderOverridePlugin() {
+  const fs = require("node:fs");
+
+  return {
+    name: loaderOverridePlugin.name,
+    setup(build) {
+      build.onResolve({ filter: /loader=.*/ }, (args) => {
+        return {
+          namespace: loaderOverridePlugin.name,
+          path: args.path,
+          pluginData: args,
+        };
+      });
+      build.onLoad(
+        { namespace: loaderOverridePlugin.name, filter: /.*/ },
+        async (args) => {
+          const importPath = args.path.split("?")[0];
+          const resolveDir = args.pluginData.resolveDir;
+          const resolvedPath = require.resolve(importPath, {
+            paths: [resolveDir],
+          });
+          const contents = await fs.promises.readFile(resolvedPath, "utf8");
+          const loader = args.path.split("loader=")[1];
+          return { contents, loader };
+        }
+      );
+    },
+  };
+}
