544716c1173dc9a1dbfe66c5b36a84b6994404af
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-09 09:48:48 +0900

    chore: comment

--- app/utils/sentry-utils.client.tsx
+++ app/utils/sentry-utils.client.tsx
@@ -1,11 +1,13 @@
 import { init } from "@sentry/browser";
 import { publicConfig } from "./config";
 
+// cf. default integrations
+// https://github.com/getsentry/sentry-javascript/blob/b624bff35854ea0497f0ea07425719723a73a79b/packages/node/src/sdk.ts#L38-L57
+
 export function initializeSentryBrowser() {
   init({
     dsn: publicConfig.APP_SENTRY_DSN,
     environment: `${publicConfig.VERCEL_ENV ?? "local"}-client`,
     release: "local",
-    // integrations: [],
   });
 }
--- app/utils/sentry-utils.server.tsx
+++ app/utils/sentry-utils.server.tsx
@@ -1,11 +1,13 @@
 import { init } from "@sentry/node";
 import { publicConfig } from "./config";
 
+// cf. default integration
+// https://github.com/getsentry/sentry-javascript/blob/b624bff35854ea0497f0ea07425719723a73a79b/packages/browser/src/sdk.ts#L26-L35
+
 export function initializeSentryServer() {
   init({
     dsn: publicConfig.APP_SENTRY_DSN,
     environment: `${publicConfig.VERCEL_ENV ?? "local"}-server`,
     release: "local",
-    // integrations: [],
   });
 }
