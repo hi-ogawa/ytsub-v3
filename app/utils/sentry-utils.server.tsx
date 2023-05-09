56244cdd7da58f2289fcb4c247b0f808a454df00
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-09 13:26:24 +0900

    chore: add CaptureConsole integration on server

--- app/utils/sentry-utils.server.tsx
+++ app/utils/sentry-utils.server.tsx
@@ -1,3 +1,4 @@
+import { CaptureConsole } from "@sentry/integrations";
 import { init } from "@sentry/node";
 import { buildConfig, publicConfig } from "./config";
 
@@ -9,5 +10,9 @@ export function initializeSentryServer() {
     dsn: publicConfig.APP_SENTRY_DSN,
     environment: `${publicConfig.VERCEL_ENV ?? "local"}-server`,
     release: buildConfig.GIT_COMMIT_REF ?? "local",
+    // auto report console.error e.g.
+    // - trpc error in app/routes/trpc/$trpc.tsx
+    // - remix loader/ssr error https://github.com/remix-run/remix/blob/410e9d8aac044b3453c1dddbeb0bcc1d53cdec68/packages/remix-server-runtime/server.ts#L169
+    integrations: [new CaptureConsole()],
   });
 }
