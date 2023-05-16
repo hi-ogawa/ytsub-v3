5925179aeff5a72b470f5a86900f9b27fb0c475d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-16 13:14:14 +0900

    chore: tweak react-query

--- app/utils/react-query-utils.tsx
+++ app/utils/react-query-utils.tsx
@@ -1,13 +1,18 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
+import { toast } from "react-hot-toast";
 import { trpc } from "../trpc/client";
 import { useDocumentEvent } from "./hooks-client-utils";
 
 export function QueryClientWrapper({ children }: React.PropsWithChildren) {
   const [queryClient] = React.useState(() => createQueryClient());
 
-  const [showDevtools, setShowDevtools] = React.useState(false);
+  const [showDevtools, setShowDevtools] = React.useState(true);
 
   // alt + shift + q to toggle
   useDocumentEvent("keyup", (e) => {
@@ -34,6 +39,12 @@ function createQueryClient() {
         refetchOnReconnect: false,
       },
     },
+    queryCache: new QueryCache({
+      onError(error, _query) {
+        console.error(error);
+        toast.error("Something went wrong...");
+      },
+    }),
   });
 
   queryClient.setQueryDefaults([trpc.videos_getCaptionEntries.queryKey], {
