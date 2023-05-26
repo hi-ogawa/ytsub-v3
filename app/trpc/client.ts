d66958a5d9c218705fd3cff2efdbe280277bdfed
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-26 13:21:42 +0900

    chore: lint

--- app/trpc/client.ts
+++ app/trpc/client.ts
@@ -11,7 +11,7 @@ import type { trpcApp } from "./server";
 type Inputs = inferRouterInputs<typeof trpcApp>;
 type Outputs = inferRouterOutputs<typeof trpcApp>;
 
-export { Inputs as TrpcInputs, Outputs as TrpcOutpus };
+export { Inputs as TrpcInputs };
 
 type ReactQueryIntegration = {
   [K in keyof Inputs]: {
