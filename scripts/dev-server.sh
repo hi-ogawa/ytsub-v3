0145c088d8743a018ed3ae55a5ca87a31e3c75cd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-26 20:52:25 +0900

    test: fix e2e coverage

--- scripts/dev-server.sh
+++ scripts/dev-server.sh
@@ -5,4 +5,8 @@ server_entry="./build/remix/${NODE_ENV:-development}/server/index.js"
 
 bash scripts/wait-for.sh test -f "$server_entry"
 
-exec nodemon --enable-source-maps "$server_entry" --watch "$server_entry"
+if [ -n "$E2E_COVERAGE_SERVER" ]; then
+  exec npx c8 -o coverage/e2e-server -r text -r html --exclude build --exclude-after-remap node --enable-source-maps "$server_entry"
+else
+  exec npx nodemon --enable-source-maps "$server_entry" --watch "$server_entry"
+fi
--- scripts/test-e2e-coverage.sh
+++ scripts/test-e2e-coverage.sh
@@ -4,6 +4,7 @@ set -eux -o pipefail
 export NODE_ENV=test
 export PORT=3001
 export E2E_NO_SERVER=1
+export E2E_COVERAGE_SERVER=1
 export E2E_COVERAGE_CLIENT=1
 
 log_file=logs/remix-coverage.log
@@ -11,7 +12,7 @@ trap 'cat "${log_file}"' EXIT
 
 # run remix server with c8
 pnpm dev-pre
-pnpm dev-coverage:remix > "$log_file" 2>&1 &
+pnpm dev:remix > "$log_file" 2>&1 &
 coverage_pid="$!"
 
 # wait server
@@ -24,7 +25,7 @@ playwright test "${@}"
 npx c8 report -o coverage/e2e-client -r text -r html --exclude build --exclude-after-remap --temp-directory coverage/e2e-client/tmp
 
 # stop remix server
-curl "http://localhost:$PORT/dev/stop"
+curl "http://localhost:$PORT/dev/stop" || true
 
 # wait for c8 to create e2e-server coverage
 wait "$coverage_pid" || true
