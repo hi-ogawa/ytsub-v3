038ab01b69b8ad7dcaeb407a7e6a0cff5e16eab3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-28 20:34:58 +0900

    chore: fix dev-server.sh

--- scripts/dev-server.sh
+++ scripts/dev-server.sh
@@ -5,8 +5,10 @@ server_entry="./build/remix/${NODE_ENV:-development}/server/index.js"
 
 bash scripts/wait-for.sh test -f "$server_entry"
 
+entry_dev_cmd=(node -r esbuild-register ./app/server/entry-dev.ts "$server_entry")
+
 if [ -n "${E2E_COVERAGE_SERVER:-}" ]; then
-  exec npx c8 -o coverage/e2e-server -r text -r html --exclude build --exclude-after-remap node -- -r esbuild-register ./app/server/entry-dev.ts "$server_entry"
+  exec npx c8 -o coverage/e2e-server -r text -r html --exclude build --exclude-after-remap "${entry_dev_cmd[@]}"
 else
-  exec npx node -r esbuild-register ./app/server/entry-dev.ts "$server_entry"
+  exec "${entry_dev_cmd[@]}"
 fi
