89ed6eb57799ab6d0bebd7a72a1979bc8fd235ea
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-05-30 12:25:37 +0900

    feat: add /videos/caption-editor page

--- app/routes/videos/caption-editor.tsx
+++ app/routes/videos/caption-editor.tsx
@@ -1,3 +1,40 @@
+import { tinyassert } from "@hiogawa/utils";
+import { CaptionEditor } from "../../components/caption-editor";
+import { ROUTE_DEF } from "../../misc/routes";
+import { useLoaderDataExtra } from "../../utils/loader-utils";
+import { makeLoader } from "../../utils/loader-utils.server";
+import type { PageHandle } from "../../utils/page-handle";
+import { parseVideoId } from "../../utils/youtube";
+
+export const handle: PageHandle = {
+  navBarTitle: () => "Caption editor",
+};
+
+//
+// loader
+//
+
+type LoaderData = {
+  videoId: string;
+};
+
+export const loader = makeLoader(async ({ ctx }) => {
+  const query = ROUTE_DEF["/videos/caption-editor"].query.parse(ctx.query);
+  const videoId = parseVideoId(query.videoId);
+  tinyassert(videoId);
+  return { videoId } satisfies LoaderData;
+});
+
+//
+// component
+//
+
 export default function Page() {
-  return <div>TODO: caption editor</div>;
+  const { videoId } = useLoaderDataExtra() as LoaderData;
+
+  return (
+    <div className="p-2 h-full">
+      <CaptionEditor videoId={videoId} />
+    </div>
+  );
 }
