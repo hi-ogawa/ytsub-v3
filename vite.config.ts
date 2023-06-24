060fa7ac6e5df4d8169236d104e65b719ba18bab
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-24 16:24:53 +0900

    refactor: simplify injectThemeScriptPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,32 @@
 import fs from "node:fs";
 import react from "@vitejs/plugin-react";
 import unocss from "unocss/vite";
-import { defineConfig } from "vite";
+import { Plugin, defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [unocss(), react(), injectHtmlPlugin()],
+  plugins: [unocss(), react(), injectThemeScriptPlugin()],
   clearScreen: false,
   publicDir: false,
 });
 
 // inject theme initialization script
-function injectHtmlPlugin() {
+function injectThemeScriptPlugin(): Plugin {
   const script = fs.readFileSync(
     require.resolve("@hiogawa/utils-experimental/dist/theme-script.global.js"),
     "utf-8"
   );
   return {
-    name: "local:" + injectHtmlPlugin.name,
-    transformIndexHtml(html: string) {
-      return html.replace(
-        /<!--@@INJECT_THEME_SCRIPT@@-->/,
-        `\
-<script>
-  globalThis.__themeStorageKey = "ytsub:theme";
-  ${script}
-</script>`
-      );
+    name: "local:" + injectThemeScriptPlugin.name,
+    transformIndexHtml() {
+      return [
+        {
+          tag: "script",
+          children: `
+            globalThis.__themeStorageKey = "ytsub:theme";
+            ${script}
+          `,
+        },
+      ];
     },
   };
 }
