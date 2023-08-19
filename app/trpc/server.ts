f3573ccbac20ee34f9be15023213dbf7e88e2f4e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-19 13:11:34 +0900

    chore: remove unused

--- app/trpc/server.ts
+++ app/trpc/server.ts
@@ -1,9 +1,7 @@
 import { routerFactory } from "./factory";
 import { trpcRoutesDecks } from "./routes/decks";
-import { trpcRoutesVideos } from "./routes/videos";
 
 // intricate nested router is not necessary by manually "namespace"-ing procedure name
 export const trpcApp = routerFactory({
   ...trpcRoutesDecks,
-  ...trpcRoutesVideos,
 });
