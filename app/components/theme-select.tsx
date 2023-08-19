20a86af85bed642741c46dd2fc4da7f509481a4a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-19 16:04:14 +0900

    chore(deps): cleanup utils packages

--- app/components/theme-select.tsx
+++ app/components/theme-select.tsx
@@ -1,10 +1,7 @@
+import { getTheme, setTheme } from "@hiogawa/theme-script";
 import { capitalize } from "@hiogawa/utils";
 import React from "react";
 import { SelectWrapper } from "./misc";
-
-// by @hiogawa/utils-experimental/dist/theme-script.global.js
-declare let __themeSet: (theme: string) => void;
-declare let __themeGet: () => string;
 
 const THEME_OPTIONS = ["system", "dark", "light"];
 
@@ -18,20 +15,13 @@ export function ThemeSelect() {
       <SelectWrapper
         className="antd-input p-1"
         options={THEME_OPTIONS}
-        value={__themeGet()}
+        value={getTheme()}
         labelFn={(v) => capitalize(v)}
         onChange={(selected) => {
-          __themeSet(selected);
+          setTheme(selected);
           rerender();
         }}
       />
     </label>
   );
-}
-
-export function injectThemeScript() {
-  return `\
-globalThis.__themeStorageKey = "ytsub:theme";
-${require("@hiogawa/utils-experimental/dist/theme-script.global.js?loader=text")}
-`;
 }
