5392b5073297315ea189b39929993d70c7daab07
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-20 03:25:47 +0900

    chore: remove obsolete PureCommentPlugin

--- remix.config.js
+++ remix.config.js
@@ -26,43 +26,8 @@ module.exports = {
 // see patches/@remix-run__dev
 //
 globalThis.__esbuildPluginsCommon = [loaderOverridePlugin()];
-globalThis.__esbuildPluginsBrowser = [pureCommentPlugin()];
+globalThis.__esbuildPluginsBrowser = [];
 globalThis.__esbuildPluginsServer = [];
-
-// tree-shake `export const loader = makeLoader(...)` from client bundle
-function pureCommentPlugin() {
-  const fs = require("node:fs");
-  const path = require("node:path");
-
-  return {
-    name: pureCommentPlugin.name,
-    setup(build) {
-      build.onLoad(
-        {
-          namespace: "file",
-          filter: new RegExp(
-            `${__dirname}/app/`.replace(/[.*+?^${}()|[\]\\]/g, "\\$&") +
-              ".*.tsx"
-          ),
-        },
-        async (args) => {
-          let contents = await fs.promises.readFile(args.path, "utf8");
-          if (contents.includes("makeLoader(")) {
-            contents = contents.replace(
-              "makeLoader(",
-              "/* @__PURE__ */ makeLoader("
-            );
-            return {
-              contents,
-              resolveDir: path.dirname(args.path),
-              loader: "tsx",
-            };
-          }
-        }
-      );
-    },
-  };
-}
 
 // immitate raw loader by require("some-file?loader=text")
 function loaderOverridePlugin() {
