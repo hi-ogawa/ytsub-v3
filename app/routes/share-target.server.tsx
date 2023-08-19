002dea80551962cb3b0c7f148bde1a6bba71375f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-20 03:11:20 +0900

    chore: contd

---
+++ app/routes/share-target.server.tsx
@@ -0,0 +1,12 @@
+import { tinyassert } from "@hiogawa/utils";
+import { redirect } from "@remix-run/server-runtime";
+import { $R, ROUTE_DEF } from "../misc/routes";
+import { makeLoader } from "../utils/loader-utils.server";
+import { parseVideoId } from "../utils/youtube";
+
+export const loader = makeLoader(async ({ ctx }) => {
+  const query = ROUTE_DEF["/share-target"].query.parse(ctx.query);
+  const videoId = parseVideoId(query["share-target-text"]);
+  tinyassert(videoId);
+  return redirect($R["/videos/new"](null, { videoId }));
+});
