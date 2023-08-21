2a4a256476de9621c6f3b9082866a84b74135c93
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-21 10:44:48 +0900

    chore: fix build

--- app/root.server.tsx
+++ app/root.server.tsx
@@ -1,8 +1,8 @@
 import { ctx_currentUser } from "./server/request-context/session";
 import { RootLoaderData } from "./utils/loader-utils";
-import { makeLoader } from "./utils/loader-utils.server";
+import { wrapLoader } from "./utils/loader-utils.server";
 
-export const loader = makeLoader(async () => {
+export const loader = wrapLoader(async () => {
   const loaderData: RootLoaderData = {
     currentUser: await ctx_currentUser(),
   };
