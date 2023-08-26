fabbff59382da44059c92784d34812389d055f68
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-08-26 12:06:11 +0900

    chore: simplify loader test

--- app/utils/loader-utils.server.ts
+++ app/utils/loader-utils.server.ts
@@ -17,8 +17,8 @@ export function wrapLoader(loader: () => unknown) {
 }
 
 export function wrapLoaderV2(loader: () => unknown) {
-  return async ({ params }: LoaderArgs) => {
-    ctx_get().params = params;
+  return async (args?: Partial<LoaderArgs>) => {
+    ctx_get().params = args?.params ?? {};
     const res = await loader();
     return res instanceof Response ? res : json(JSON_EXTRA.serialize(res));
   };
