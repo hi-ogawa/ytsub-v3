84fff0e8b62ed767d9dd719d33a86f6e0100f67e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-09-16 15:43:39 +0900

    chore: comment

--- app/utils/form-utils.ts
+++ app/utils/form-utils.ts
@@ -1,5 +1,10 @@
 import type React from "react";
 
+// forcing controled input value is tricky in general
+// cf.
+// https://github.com/solidjs/solid/issues/1756
+// https://technology.blog.gov.uk/2020/02/24/why-the-gov-uk-design-system-team-changed-the-input-type-for-numbers/
+
 export function asNumberInput(props: {
   name: string;
   value: number;
