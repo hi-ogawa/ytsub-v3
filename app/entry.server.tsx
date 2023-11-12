7cfcd10374a585c6ac77af8a2811ebe7017070fd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-12 14:27:16 +0900

    fix: inject css manually for prod

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,6 +3,8 @@ import type { HandleDocumentRequestFunction } from "@remix-run/server-runtime";
 import { renderToString } from "react-dom/server";
 import { renderToDocument } from "./server/document";
 import { wrapTraceAsyncSimple } from "./utils/opentelemetry-utils";
+import { tinyassert } from "@hiogawa/utils";
+import { viteDevServer } from "@hiogawa/vite-import-dev-server/runtime";
 
 const handleDocumentRequest: HandleDocumentRequestFunction = async (
   request,
@@ -14,7 +16,24 @@ const handleDocumentRequest: HandleDocumentRequestFunction = async (
   const ssrHtml = renderToString(
     <RemixServer context={remixContext} url={request.url} />
   );
-  const documentHtml = await renderToDocument(ssrHtml);
+
+  let style: string
+  if (import.meta.env.DEV) {
+    // inject CSS to quickly workaround FOUC during dev
+    // since vite/unocss will inject css on client via javascript later.
+    // this would essentially create a duplicate style,
+    // but that's not usually a problem for utility-class based styling.
+    tinyassert(viteDevServer);
+    const unocss = await viteDevServer.ssrLoadModule("virtual:uno.css");
+    style = `<style>${unocss["default"]}</style>`;
+  } else {
+    // since we don't use <Links />, we have to inject unocss output manually.
+    const root = remixContext.manifest.routes["root"];
+    tinyassert(root && root.css && root.css[0]);
+    style = `<link rel="stylesheet" href="${root.css[0]}">`;
+  }
+
+  const documentHtml = await renderToDocument(ssrHtml, style);
   responseHeaders.set("content-type", "text/html");
   return new Response(documentHtml, {
     status: responseStatusCode,
--- app/server/document.tsx
+++ app/server/document.tsx
@@ -1,23 +1,10 @@
 import { generateThemeScript } from "@hiogawa/theme-script";
-import { tinyassert } from "@hiogawa/utils";
-import { viteDevServer } from "@hiogawa/vite-import-dev-server/runtime";
 import { injectPublicConfigScript, publicConfig } from "../utils/config-public";
 
 // since we don't currently use remix's <Meta /> or <Links /> convention,
 // we can render static document html only on server, which is probably common ssr practice.
 
-export async function renderToDocument(ssrHtml: string) {
-  // inject CSS to quickly workaround FOUC during dev
-  // since vite/unocss will inject css on client via javascript later.
-  // this would essentially create a duplicate style,
-  // but that's not usually a problem for utility-class based styling.
-  let style = "";
-  if (import.meta.env.DEV) {
-    tinyassert(viteDevServer);
-    const unocss = await viteDevServer.ssrLoadModule("virtual:uno.css");
-    style = `<style>${unocss["default"]}</style>`;
-  }
-
+export async function renderToDocument(ssrHtml: string, style: string) {
   // syntax highlight by https://github.com/mjbvz/vscode-comment-tagged-templates/
   return /* html */ `
 <!DOCTYPE html>
