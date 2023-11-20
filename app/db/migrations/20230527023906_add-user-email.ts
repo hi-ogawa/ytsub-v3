d40b421a18d66420bafb6a9cee8a0a816b76873c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-20 14:29:01 +0900

    fix: struggle with cwd

--- app/db/migrations/20230527023906_add-user-email.ts
+++ app/db/migrations/20230527023906_add-user-email.ts
@@ -4,7 +4,7 @@ import type { Knex } from "knex";
 export async function up(knex: Knex) {
   await knex.raw(
     await fs.promises.readFile(
-      "app/db/migrations/raw/20230527023906_add-user-email/up.sql",
+      "raw/20230527023906_add-user-email/up.sql",
       "utf-8"
     )
   );
@@ -13,7 +13,7 @@ export async function up(knex: Knex) {
 export async function down(knex: Knex) {
   await knex.raw(
     await fs.promises.readFile(
-      "app/db/migrations/raw/20230527023906_add-user-email/down.sql",
+      "raw/20230527023906_add-user-email/down.sql",
       "utf-8"
     )
   );
