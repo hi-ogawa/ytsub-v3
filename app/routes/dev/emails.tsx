aa3274bc48e6606d200d1aa3b25fb59e5fc409fe
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-12-27 17:25:43 +0900

    chore: test path alias

--- app/routes/dev/emails.tsx
+++ app/routes/dev/emails.tsx
@@ -1,6 +1,6 @@
+import { debugEmails } from "#utils/email-utils";
+import { prettierJson } from "#utils/loader-utils";
 import type { LoaderFunction } from "@remix-run/server-runtime";
-import { debugEmails } from "../../utils/email-utils";
-import { prettierJson } from "../../utils/loader-utils";
 
 export const loader: LoaderFunction = async () => {
   return prettierJson(debugEmails);
